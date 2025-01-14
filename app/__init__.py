0869172e5b1f92223d11d10a84193f67a84df4c1
Shann <shann.vana@gmail.com>
2025-01-14 22:51:28 +0300

    Add import os and integrate Flask-Migrate for database migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -29,14 +30,13 @@ def create_app(config_name='development'):
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_DEFAULT_SENDER'] = 'noreply@techconnect.com' 
 
-
     from .routes import routes
     app.register_blueprint(routes)
     
     # Initialize database, mail, login manager with the app
     db.init_app(app)
-    login_manager.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
     mail.init_app(app)
     
     # Set up the login view for unauthenticated users
